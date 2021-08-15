0f01cea104ec9d540120708733addd9c9ce89105
Caster <Casterscz@163.com>
2021-08-15 23:04:51 +0800

    ScripteCollection 完善 可以添加Sprite 文件夹  mulitSprite

--- Assets/Scripts/SpriteCollectionExtension/Editor/SpriteCollectionEditor.cs
+++ Assets/Scripts/SpriteCollectionExtension/Editor/SpriteCollectionEditor.cs
@@ -1,55 +1,355 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
 using GameFramework;
 using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace UGFExtensions.Editor
 {
     [CustomEditor(typeof(SpriteCollection))]
     public class SpriteCollectionEditor : UnityEditor.Editor
     {
+        private SpriteCollection Target => target as SpriteCollection;
         private SerializedProperty m_Sprites;
-        private Sprite m_Sprite;
-        private SpriteCollection m_SpriteCollection;
+        private SerializedProperty m_Objects;
+        private SerializedProperty m_Keys;
+        private SerializedProperty m_Values;
+        private ReorderableList m_ReorderableList;
+        private readonly int m_SelectorHash = "ObjectSelector".GetHashCode();
+        private bool m_PackableListExpanded = true;
 
         private void OnEnable()
         {
-            m_SpriteCollection = target as SpriteCollection;
             m_Sprites = serializedObject.FindProperty("m_Sprites");
-            // m_Sprite = serializedObject.FindProperty("m_Sprite");
+            m_Objects = serializedObject.FindProperty("m_Objects");
+            m_Keys = m_Sprites.FindPropertyRelative("m_keys");
+            m_Values = m_Sprites.FindPropertyRelative("m_values");
+            m_ReorderableList = new ReorderableList(serializedObject, m_Objects, true, true, true, true)
+            {
+                onAddCallback = AddPackable,
+                onRemoveCallback = RemovePackable,
+                drawElementCallback = DrawPackableElement,
+                elementHeight = EditorGUIUtility.singleLineHeight,
+                headerHeight = 0f,
+            };
+        }
+
+        void AddPackable(ReorderableList list)
+        {
+            EditorGUIUtility.ShowObjectPicker<Object>(null, false, "t:sprite t:texture2d t:folder", m_SelectorHash);
+        }
+
+        void RemovePackable(ReorderableList list)
+        {
+            var index = list.index;
+            if (index != -1)
+            {
+                Object obj = m_Objects.GetArrayElementAtIndex(index).objectReferenceValue;
+                string path = AssetDatabase.GetAssetPath(obj);
+                if (obj is Sprite)
+                {
+                    RemoveDictionary(path);
+                }
+                else if (obj is Texture2D)
+                {
+                    var objs = AssetDatabase.LoadAllAssetsAtPath(path);
+                    if (objs.Length == 2)
+                    {
+                        RemoveDictionary(path);
+                    }
+                    else
+                    {
+                        for (int i = 1; i < objs.Length; i++)
+                        {
+                            if (objs[i] is Sprite)
+                            {
+                                string regularPath = Utility.Path.GetRegularPath(Path.Combine(path, objs[i].name));
+                                RemoveDictionary(regularPath);
+                            }
+                        }
+                    }
+                }
+                else if ((obj is DefaultAsset && ProjectWindowUtil.IsFolder(obj.GetInstanceID())))
+                {
+                    string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
+                        .Where(f => !f.EndsWith(".meta", StringComparison.Ordinal)).ToArray();
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        var objs = AssetDatabase.LoadAllAssetsAtPath(files[i]);
+                        if (objs.Length == 2)
+                        {
+                            string regularPath = Utility.Path.GetRegularPath(files[i]);
+                            RemoveDictionary(regularPath);
+                        }
+                        else
+                        {
+                            for (int j = 1; j < objs.Length; j++)
+                            {
+                                if (objs[j] is Sprite)
+                                {
+                                    string regularPath =
+                                        Utility.Path.GetRegularPath(Path.Combine(files[i], objs[j].name));
+                                    RemoveDictionary(regularPath);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                m_Objects.GetArrayElementAtIndex(index).objectReferenceValue = null;
+                m_Objects.DeleteArrayElementAtIndex(index);
+                // ReorderableList.defaultBehaviours.DoRemoveButton(list);
+                // ReorderableList.defaultBehaviours.DoRemoveButton(list);
+            }
         }
 
         public override void OnInspectorGUI()
         {
             serializedObject.Update();
-            EditorGUILayout.PropertyField(m_Sprites, true);
-            // EditorGUILayout.ObjectField(m_Sprite,typeof(Sprite),false);
-            if (GUILayout.Button("Collect Sprites"))
+            GUI.enabled = false;
+            EditorGUILayout.PropertyField(m_Sprites);
+            GUI.enabled = true;
+
+            HandlePackableListUI();
+            DrawPackUI();
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void HandlePackableListUI()
+        {
+            var currentEvent = Event.current;
+            var usedEvent = false;
+            Rect rect = EditorGUILayout.GetControlRect();
+            var controlID = GUIUtility.GetControlID(FocusType.Passive);
+            switch (currentEvent.type)
             {
-                string[] imageType = 
-                {
-                    "*.JPG",
-                    "*.PNG"
-                };
-                for (int i = 0; i < imageType.Length; i++) {
-                    string[] dirs = Directory.GetFiles (Application.dataPath, imageType [i],SearchOption.AllDirectories);  
-                    
-                    for (int j = 0; j < dirs.Length; j++)
+                case EventType.DragUpdated:
+                case EventType.DragPerform:
+                    if (rect.Contains(currentEvent.mousePosition) && GUI.enabled)
                     {
-                        string path = Utility.Path.GetRegularPath(dirs[j]);
-                        int index = path.IndexOf(@"Assets/", StringComparison.Ordinal);
-                        path =path.Substring(index);
-                        m_SpriteCollection.AddSprite(path,AssetDatabase.LoadAssetAtPath<Sprite>(path));
-                    }  
-                }
-                
+                        // Check each single object, so we can add multiple objects in a single drag.
+                        var didAcceptDrag = false;
+                        var references = DragAndDrop.objectReferences;
+                        foreach (var obj in references)
+                        {
+                            if (IsPackable(obj))
+                            {
+                                DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+                                if (currentEvent.type == EventType.DragPerform)
+                                {
+                                    AddObject(obj);
+                                    didAcceptDrag = true;
+                                    DragAndDrop.activeControlID = 0;
+                                }
+                                else
+                                    DragAndDrop.activeControlID = controlID;
+                            }
+                        }
+
+                        if (didAcceptDrag)
+                        {
+                            GUI.changed = true;
+                            DragAndDrop.AcceptDrag();
+                            usedEvent = true;
+                        }
+                    }
+
+                    break;
+                case EventType.ExecuteCommand:
+                    if (currentEvent.commandName == "ObjectSelectorClosed" &&
+                        EditorGUIUtility.GetObjectPickerControlID() == m_SelectorHash)
+                    {
+                        var obj = EditorGUIUtility.GetObjectPickerObject();
+                        if (IsPackable(obj))
+                        {
+                            AddObject(obj);
+                            m_ReorderableList.index = m_Objects.arraySize - 1;
+                        }
+                    }
+
+                    usedEvent = true;
+                    break;
             }
 
-            if (GUILayout.Button("Clear All"))
+            if (usedEvent)
+                currentEvent.Use();
+            m_PackableListExpanded = EditorGUI.Foldout(rect, m_PackableListExpanded,
+                EditorGUIUtility.TrTextContent("Objects for Packing",
+                    "Only accept Folder, Sprite Sheet(Texture) and Sprite."), true);
+
+            if (m_PackableListExpanded)
             {
-                m_SpriteCollection.Clear();
+                EditorGUI.indentLevel++;
+                m_ReorderableList.DoLayoutList();
+                EditorGUI.indentLevel--;
+            }
+        }
+
+        private void AddObject(Object obj)
+        {
+            m_Objects.InsertArrayElementAtIndex(m_Objects.arraySize);
+            m_Objects.GetArrayElementAtIndex(m_Objects.arraySize - 1).objectReferenceValue = obj;
+            AddSprite(obj);
+        }
+
+        private void AddSprite(Object obj)
+        {
+            string path = AssetDatabase.GetAssetPath(obj);
+            if (obj is Sprite sprite)
+            {
+                AddToDictionary(path, sprite);
+            }
+            else if (obj is Texture2D)
+            {
+                var objs = AssetDatabase.LoadAllAssetsAtPath(path);
+                if (objs.Length == 2)
+                {
+                    AddToDictionary(path, objs[1] as Sprite);
+                }
+                else
+                {
+                    for (int i = 1; i < objs.Length; i++)
+                    {
+                        if (objs[i] is Sprite)
+                        {
+                            string regularPath = Utility.Path.GetRegularPath(Path.Combine(path, objs[i].name));
+                            AddToDictionary(regularPath, objs[i] as Sprite);
+                        }
+                    }
+                }
+            }
+            else if ((obj is DefaultAsset && ProjectWindowUtil.IsFolder(obj.GetInstanceID())))
+            {
+                string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories)
+                    .Where(f => !f.EndsWith(".meta", StringComparison.Ordinal)).ToArray();
+                for (int i = 0; i < files.Length; i++)
+                {
+                    var objs = AssetDatabase.LoadAllAssetsAtPath(files[i]);
+                    if (objs.Length == 2)
+                    {
+                        string regularPath = Utility.Path.GetRegularPath(files[i]);
+                        AddToDictionary(regularPath, objs[1] as Sprite);
+                        AddToDictionary(regularPath, objs[1] as Sprite);
+                    }
+                    else
+                    {
+                        for (int j = 1; j < objs.Length; j++)
+                        {
+                            if (objs[j] is Sprite)
+                            {
+                                string regularPath = Utility.Path.GetRegularPath(Path.Combine(files[i], objs[j].name));
+                                AddToDictionary(regularPath, objs[1] as Sprite);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void AddToDictionary(string path, Sprite sprite)
+        {
+            m_Keys.InsertArrayElementAtIndex(m_Keys.arraySize);
+            m_Keys.GetArrayElementAtIndex(m_Keys.arraySize - 1).stringValue = path;
+            m_Values.InsertArrayElementAtIndex(m_Values.arraySize);
+            m_Values.GetArrayElementAtIndex(m_Values.arraySize - 1).objectReferenceValue = sprite;
+        }
+
+        private void RemoveDictionary(string path)
+        {
+            int index = -1;
+            for (int i = 0; i < m_Keys.arraySize; i++)
+            {
+                if (m_Keys.GetArrayElementAtIndex(i).stringValue == path)
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index != -1)
+            {
+                m_Keys.DeleteArrayElementAtIndex(index);
+                m_Values.DeleteArrayElementAtIndex(index);
+                Target.Sprites.Remove(path);
+            }
+        }
+
+        void DrawPackableElement(Rect rect, int index, bool selected, bool focused)
+        {
+            var property = m_Objects.GetArrayElementAtIndex(index);
+            var controlID = GUIUtility.GetControlID(FocusType.Passive, rect);
+            EditorGUI.BeginChangeCheck();
+            var changedObject = EditorGUI.ObjectField(rect, property.objectReferenceValue, typeof(Object), false);
+            if (EditorGUI.EndChangeCheck())
+            {
+                property.objectReferenceValue = changedObject;
+            }
+
+            if (GUIUtility.keyboardControl == controlID && !selected)
+                m_ReorderableList.index = index;
+        }
+
+        static bool IsPackable(Object o)
+        {
+            return o != null && (o is Sprite || o is Texture2D ||
+                                 (o is DefaultAsset && ProjectWindowUtil.IsFolder(o.GetInstanceID())));
+        }
+
+        string DictionaryToString<K, V>(IDictionary<K, V> dictionary)
+        {
+            var stringBuilder = new StringBuilder();
+            var comma = ",";
+            var index = 0;
+            foreach (var keyValue in dictionary)
+            {
+                var separator = index < dictionary.Count - 1 ? comma : string.Empty;
+                stringBuilder.Append($"{{{keyValue.Key.ToString()},{keyValue.Value.ToString()}}}{separator}");
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        private void DrawPackUI()
+        {
+            if (GUILayout.Button("Package", GUILayout.ExpandWidth(false)))
+            {
+                Target.Sprites.Clear();
+                m_Keys.ClearArray();
+                m_Values.ClearArray();
+                for (int i = 0; i < m_Objects.arraySize; i++)
+                {
+                    Object obj = m_Objects.GetArrayElementAtIndex(i).objectReferenceValue;
+                    if (obj == null)
+                    {
+                        m_Objects.DeleteArrayElementAtIndex(i);
+                        m_Objects.DeleteArrayElementAtIndex(i);
+                    }
+
+                    AddSprite(obj);
+                }
+            }
+        }
+
+        private void OnDisable()
+        {
+            Target.Sprites.Clear();
+            m_Keys.ClearArray();
+            m_Values.ClearArray();
+            for (int i = 0; i < m_Objects.arraySize; i++)
+            {
+                Object obj = m_Objects.GetArrayElementAtIndex(i).objectReferenceValue;
+                if (obj == null)
+                {
+                    m_Objects.DeleteArrayElementAtIndex(i);
+                    m_Objects.DeleteArrayElementAtIndex(i);
+                }
+
+                AddSprite(obj);
             }
 
             serializedObject.ApplyModifiedProperties();
--- Assets/Scripts/SpriteCollectionExtension/SpriteCollection.cs
+++ Assets/Scripts/SpriteCollectionExtension/SpriteCollection.cs
@@ -2,78 +2,26 @@ using System;
 using System.Collections.Generic;
 
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace UGFExtensions
 {
-    [CreateAssetMenu(fileName = "SpriteCollection", menuName = "LegacySprite/SpriteCollection", order = 0)]
+    [CreateAssetMenu(fileName = "SpriteCollection", menuName = "UGFExtensions/SpriteCollection", order = 0)]
     public class SpriteCollection : ScriptableObject
     {
-        [SerializeField] private Sprite m_Sprite;
-        [SerializeField] private StringSpriteDictionary m_Sprites = new StringSpriteDictionary();
-        public Sprite GetSprite(string spriteName)
-        {
-            m_Sprites.Sprites.TryGetValue(spriteName, out Sprite value);
-            return value;
-        }
-#if UNITY_EDITOR
-        public void AddSprite(string spritePath,Sprite sprite)
-        {
-            if (m_Sprites.Sprites.ContainsKey(spritePath))
-            {
-                Debug.LogError($"Sprite:{spritePath} is exist");
-                return;
-            }
-            m_Sprites.Add(spritePath,sprite);
-        }
+        [SerializeField] private StringSpriteDictionary m_Sprites;
+        [SerializeField] private List<Object> m_Objects;
 
-        public void Clear()
+        // public IDictionary<string, string> Sprites
+        // {
+        //     get => m_Sprites;
+        //     set => m_Sprites = value;
+        // }
+        public IDictionary<string, Sprite> Sprites
         {
-            m_Sprites.Sprites.Clear();
-            m_Sprites.Keys.Clear();
-            m_Sprites.Values.Clear();
+            get { return m_Sprites; }
+            set { m_Sprites.CopyFrom (value); }
         }
-#endif
-    }
-    [Serializable]
-    public class StringSpriteDictionary
-    {
-        [SerializeField]
-        private List<string> m_Keys;
-        [SerializeField]
-        private List<Sprite> m_Values;
-
-        private Dictionary<string, Sprite> m_Sprites;
-
-        public List<string> Keys
-        {
-            get => m_Keys;
-            set => m_Keys = value;
-        }
-
-        public List<Sprite> Values
-        {
-            get => m_Values;
-            set => m_Values = value;
-        }
-
-        public Dictionary<string, Sprite> Sprites
-        {
-            get => m_Sprites;
-            set => m_Sprites = value;
-        }
-
-        public StringSpriteDictionary()
-        {
-            m_Keys = new List<string>();
-            m_Values = new List<Sprite>();
-            m_Sprites = new Dictionary<string, Sprite>();
-        }
-
-        public void Add(string path,Sprite sprite)
-        {
-            m_Sprites.Add(path,sprite);
-            m_Keys.Add(path);
-            m_Values.Add(sprite);
-        }
+        
     }
 }
