63409b6ec71f3c035f1e4815a423055947f411d6
caster <2474252111@qq.com>
2022-03-29 16:19:54 +0800

    [修改] 移动Await 扩展中 加载数据表函数 到Starforce 部分类中

--- Assets/Scripts/AwaitExtension/AwaitableExtensions.cs
+++ Assets/Scripts/AwaitExtension/AwaitableExtensions.cs
@@ -73,57 +73,6 @@ namespace UGFExtensions.Await
 #endif
 
         /// <summary>
-        /// 加载数据表（可等待）
-        /// </summary>
-        public static async Task<IDataTable<T>> LoadDataTableAsync<T>(this DataTableComponent dataTableComponent,
-            string dataTableName, bool formBytes, object userData = null) where T : IDataRow
-        {
-#if UNITY_EDITOR
-            TipsSubscribeEvent();
-#endif
-            IDataTable<T> dataTable = dataTableComponent.GetDataTable<T>();
-            if (dataTable != null)
-            {
-                return await Task.FromResult(dataTable);
-            }
-
-            var loadTcs = new TaskCompletionSource<bool>();
-            var dataTableAssetName = AssetUtility.GetDataTableAsset(dataTableName, formBytes);
-            s_DataTableTcs.Add(dataTableAssetName, loadTcs);
-            dataTableComponent.LoadDataTable(dataTableName, dataTableAssetName, userData);
-            bool isLoaded = await loadTcs.Task;
-            dataTable = isLoaded ? dataTableComponent.GetDataTable<T>() : null;
-            return await Task.FromResult(dataTable);
-        }
-
-
-        private static void OnLoadDataTableSuccess(object sender, GameEventArgs e)
-        {
-            var ne = (LoadDataTableSuccessEventArgs)e;
-            s_DataTableTcs.TryGetValue(ne.DataTableAssetName, out TaskCompletionSource<bool> tcs);
-            if (tcs != null)
-            {
-                Log.Info("Load data table '{0}' OK.", ne.DataTableAssetName);
-                tcs.SetResult(true);
-                s_DataTableTcs.Remove(ne.DataTableAssetName);
-            }
-        }
-
-        private static void OnLoadDataTableFailure(object sender, GameEventArgs e)
-        {
-            var ne = (LoadDataTableFailureEventArgs)e;
-            s_DataTableTcs.TryGetValue(ne.DataTableAssetName, out TaskCompletionSource<bool> tcs);
-            if (tcs != null)
-            {
-                Log.Error("Can not load data table '{0}' from '{1}' with error message '{2}'.", ne.DataTableAssetName,
-                    ne.DataTableAssetName, ne.ErrorMessage);
-                tcs.SetResult(false);
-                s_DataTableTcs.Remove(ne.DataTableAssetName);
-            }
-        }
-
-        
-        /// <summary>
         /// 打开界面（可等待）
         /// </summary>
         public static Task<UIForm> OpenUIFormAsync(this UIComponent uiComponent,string uiFormAssetName, string uiGroupName, int priority, bool pauseCoveredUIForm, object userData)
