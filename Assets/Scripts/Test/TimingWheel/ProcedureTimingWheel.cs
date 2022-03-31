5b6b986bb4f2fad8675630c7140c90d59db68cbf
caster <2474252111@qq.com>
2022-03-31 15:17:42 +0800

    [修改]  修改时间轮测试流程

--- Assets/Scripts/Test/TimingWheel/ProcedureTimingWheel.cs
+++ Assets/Scripts/Test/TimingWheel/ProcedureTimingWheel.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using GameFramework.Fsm;
 using GameFramework.Procedure;
 using TimingWheel.Extensions;
-using UGFExtensions.Timer;
 using UnityEngine;
 using UnityGameFramework.Runtime;
+using Random = System.Random;
 
 
 namespace UGFExtensions
@@ -17,8 +18,11 @@ namespace UGFExtensions
             base.OnInit(procedureOwner);
         }
 
+        private long m_DelayTimes1 = 0;
+        private long m_DelayTimes2 = 0;
         private LoopTask m_LoopTask;
 
+        private int m_ID = 0;
         protected override async void OnEnter(IFsm<IProcedureManager> procedureOwner)
         {
             await Task.Delay(1000);
@@ -31,25 +35,83 @@ namespace UGFExtensions
             await GameEntry.TimingWheel.AddTaskAsync(TimeSpan.FromMilliseconds(300));
             Debug.Log($"剩余时间：{task.TimeoutMs - DateTimeHelper.GetTimestamp()}");
 
-            Test(1000);
-            Test(2000);
-            Test(300);
-            Test(800);
-            Test(812);
+            for (int i = 0; i < 10000; i++)
+            {
+                Test(i+100,i);
+            }
+            for (int i = 0; i < 10000; i++)
+            {
+                Test3(i+100,i);
+            }
+            // for (int i = 0; i < 10000; i++)
+            // {
+            //     Test3(i,i);
+            // }
 
         }
 
-        private async void Test(int delay)
+        private async void Test(int delay,int id)
         {
             var now = DateTimeHelper.GetTimestamp();
             await GameEntry.TimingWheel.AddTaskAsync(TimeSpan.FromMilliseconds(delay));
             var runTime = DateTimeHelper.GetTimestamp();
             var actualDelay2 = runTime - now;
-            Debug.Log($"Timer 起始时间：{now}，" +
+            m_DelayTimes1 += actualDelay2 - delay;
+            if (id == 9999)
+            {
+                Debug.Log($"时间轮 task 总误差 :{m_DelayTimes1}  平均误差：{m_DelayTimes1 / 10000}");
+            }
+            Debug.Log($"ID: {id}  "+
+                      $"Timer 起始时间：{now}，" +
                       $"执行时间：{runTime}，" +
                       $"预期延时：{delay}ms，" +
                       $"精确延时：{actualDelay2}ms" +
                       $"延时误差：{actualDelay2 - delay}ms");
+        }
+        
+        private void Test2(int delay,int id)
+        {
+            var now = DateTimeHelper.GetTimestamp();
+            GameEntry.TimingWheel.AddTask(TimeSpan.FromMilliseconds(delay), result =>
+            {
+                var runTime = DateTimeHelper.GetTimestamp();
+                var actualDelay2 = runTime - now;
+                m_DelayTimes1 += actualDelay2 - delay;
+                if (id == 9999)
+                {
+                    Debug.Log($"时间轮 总误差 :{m_DelayTimes1}  平均误差：{m_DelayTimes1 / 10000}");
+                }
+                // Debug.Log($"ID: {id}  "+
+                //           $"Timer 起始时间：{now}，" +
+                //           $"执行时间：{runTime}，" +
+                //           $"预期延时：{delay}ms，" +
+                //           $"精确延时：{actualDelay2}ms" +
+                //           $"延时误差：{actualDelay2 - delay}ms");
+            });
+           
+        }
+        
+        private void Test3(int delay,int id)
+        {
+            var now = DateTimeHelper.GetTimestamp();
+            GameEntry.Timer.AddOnceTimer(delay, () =>
+            {
+                var runTime = DateTimeHelper.GetTimestamp();
+                var actualDelay2 = runTime - now;
+                m_DelayTimes1 += actualDelay2 - delay;
+                if (id == 9999)
+                {
+                    Debug.Log($"红黑树 总误差 :{m_DelayTimes1}  平均误差：{m_DelayTimes1 / 10000}");
+                }
+                //
+                Debug.Log($"ID: {id}  "+
+                          $"Timer 起始时间：{now}，" +
+                          $"执行时间：{runTime}，" +
+                          $"预期延时：{delay}ms，" +
+                          $"精确延时：{actualDelay2}ms" +
+                          $"延时误差：{actualDelay2 - delay}ms");
+            });
+           
         }
 
         protected override void OnUpdate(IFsm<IProcedureManager> procedureOwner, float elapseSeconds,
