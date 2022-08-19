237e098effadfc8c447825ecb321ffd2b4b39438
caster <2474252111@qq.com>
2022-08-19 18:32:49 +0800

    [新增] 删除前缀的字段名称绑定规则(未实现)

---
+++ Assets/Scripts/ReferenceBindTool/Editor/RuleHelpers/RemovePrefixNameRuleHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ReferenceBindTool.Editor
+{
+    public class RemovePrefixNameRuleHelper : INameRuleHelper
+    {
+        public string GetDefaultFieldName(Object component)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CheckFieldNameIsInvalid(string fieldName)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
