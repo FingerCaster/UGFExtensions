711817dc77e162f2a3ba2e35574ca3855a68d8a1
caster <2474252111@qq.com>
2022-09-06 16:22:16 +0800

    [feat] 修改代码结构。删除资源目录 删除Starforce 相关代码。

---
+++ Assets/Extensions/DataTableExtension/Editor/Extensions/DataTableConfig.cs
@@ -0,0 +1,113 @@
+using System.IO;
+using System.Linq;
+using GameFramework;
+using UnityEngine;
+
+namespace DE.Editor
+{
+    /// <summary>
+    /// 数据表配置类
+    /// </summary>
+    public static class DataTableConfig
+    {
+        /// <summary>
+        /// 数据表存放文件夹路径
+        /// </summary>
+        public const string DataTableFolderPath = "Assets/Res/DataTables";
+
+        public static readonly string ExcelsFolder = $"{Application.dataPath}/../Excels/";
+
+        /// <summary>
+        /// 数据表C#实体类生成文件夹路径
+        /// </summary>
+        public const string CSharpCodePath = "Assets/Scripts/Extensions/DataTableExtension/Runtime/DataTable";
+
+        /// <summary>
+        /// 数据表C#实体类模板存放路径
+        /// </summary>
+        public const string CSharpCodeTemplateFileName = "Assets/Scripts/Extensions/DataTableExtension/Editor/Resource/DataTableCodeTemplate.txt";
+
+        /// <summary>
+        /// 数据表扩展类文件夹路径
+        /// </summary>
+        public static readonly string ExtensionDirectoryPath = "Assets/Scripts/Extensions/DataTableExtension/Runtime/Extensions";
+
+        /// <summary>
+        /// 数据表命名空间
+        /// </summary>
+        public const string NameSpace = "UGFExtensions";
+
+        /// <summary>
+        /// 数据表中使用类型 所在的所有程序集
+        /// </summary>
+        public static readonly string[] AssemblyNames =
+        {
+#if UNITY_2017_3_OR_NEWER
+            //asmdef
+            "Test",
+#endif
+            "Assembly-CSharp"
+        };
+
+        /// <summary>
+        /// 编辑器中使用到的程序集
+        /// </summary>
+        public static readonly string[] EditorAssemblyNames =
+        {
+#if UNITY_2017_3_OR_NEWER
+            "UnityGameFramework.Editor",
+            "DE.Editor",
+#endif
+            "Assembly-CSharp-Editor"
+        };
+
+        /// <summary>
+        /// 数据表文件路径
+        /// </summary>
+        public static string[] TxtFilePaths;
+
+        /// <summary>
+        /// 数据表文件名
+        /// </summary>
+        public static string[] DataTableNames;
+
+        /// <summary>
+        /// Excel表文件路径
+        /// </summary>
+        public static string[] ExcelFilePaths;
+
+        static DataTableConfig()
+        {
+            RefreshDataTables();
+        }
+
+        public static int NameRow = 1;
+        public static int TypeRow = 2;
+        public static int CommentRow = 3;
+        public static int ContentStartRow = 4;
+
+        public static int IdColumn = 1;
+
+        public static void RefreshDataTables()
+        {
+            if (Directory.Exists(DataTableFolderPath))
+            {
+                var txtFolder = new DirectoryInfo(DataTableFolderPath);
+                TxtFilePaths = txtFolder.GetFiles("*.txt", SearchOption.TopDirectoryOnly)
+                    .Select(_ => Utility.Path.GetRegularPath(_.FullName))
+                    .ToArray();
+                DataTableNames = txtFolder.GetFiles("*.txt", SearchOption.TopDirectoryOnly)
+                    .Select(file => Path.GetFileNameWithoutExtension(file.Name))
+                    .ToArray();
+            }
+
+            if (Directory.Exists(ExcelsFolder))
+            {
+                var excelFolder = new DirectoryInfo(ExcelsFolder);
+                ExcelFilePaths = excelFolder.GetFiles("*.xlsx", SearchOption.TopDirectoryOnly)
+                    .Where(_ => !_.Name.StartsWith("~$")).Select(_ => Utility.Path.GetRegularPath(_.FullName))
+                    .ToArray();
+            }
+        }
+    }
+}
---
+++ Assets/Extensions/DataTableExtension/Runtime/LoadRowExtension/DataProvider/CustomVirtualFileSystemDataProvider.cs
@@ -0,0 +1,63 @@
+﻿using GameFramework.FileSystem;
+using UnityGameFramework.Runtime;
+
+namespace UGFExtensions
+{
+    /// <summary>
+    /// 自定义虚拟文件系统数据提供(mergeAsset扩展合并的vfs文件  或者自行合并的vfs文件)
+    /// </summary>
+    public class CustomVirtualFileSystemDataProvider : IDataProvider
+    {
+        private string m_FilePath = null;
+        private string m_AssetPath = null;
+        private bool m_IsCached = false;
+        private IFileSystem m_FileSystem = null;
+        private FileSystemComponent m_FileSystemComponent;
+        public CustomVirtualFileSystemDataProvider(FileSystemComponent fileSystemComponent,string filePath, string assetPath, bool isCached)
+        {
+            m_FileSystemComponent = fileSystemComponent;
+            m_FilePath = filePath;
+            m_AssetPath = assetPath;
+            m_IsCached = isCached;
+        }
+
+        private IFileSystem GetOrCreateFileSystem()
+        {
+            if (m_FileSystem != null)
+            {
+                return m_FileSystem;
+            }
+
+            var fileSystem = m_FileSystemComponent.LoadFileSystem(m_FilePath, FileSystemAccess.Read);
+            if (m_IsCached)
+            {
+                m_FileSystem = fileSystem;
+            }
+
+            return m_FileSystem;
+        }
+        
+        public long ReadFileSegment(int offset, ref byte[] buffer, int startIndex, int length)
+        {
+            var fileSystem = GetOrCreateFileSystem();
+            DataProviderUtility.EnsureBufferSize(startIndex+length, ref buffer);
+            var realLength = fileSystem.ReadFileSegment(m_AssetPath,offset,buffer, startIndex, length);
+            if (!m_IsCached)
+            {
+                m_FileSystemComponent.DestroyFileSystem(fileSystem,false);
+            }
+
+            return realLength;
+        }
+
+        public void Dispose()
+        {
+            m_FilePath = null;
+            m_AssetPath = null;
+            m_IsCached = false;
+            m_FileSystemComponent.DestroyFileSystem(m_FileSystem,false);
+            m_FileSystemComponent = null;
+            m_FileSystem = null;
+        }
+    }
+}
